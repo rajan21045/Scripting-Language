22815eed42f95fc17eecb6ec6906862e79fff3f0
Rajan Poudel <raajan.works@gmail.com>
2025-11-07 14:19:20 +0545

    Delete PHP/superglobalvariable.php

--- PHP/superglobalvariable.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "GET"){
-    $name = isset($_GET['name']) ? htmlspecialchars($_GET['name']) : 'Unknown';
-    $age = isset($_GET['age']) ? htmlspecialchars($_GET['age']) : 'Unknown';
-    echo "your name is " . $name . " and your age is " . $age . ".";
-}
-?>
