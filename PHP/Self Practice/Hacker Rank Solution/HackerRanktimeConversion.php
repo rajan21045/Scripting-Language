d7354b489a47bcae258cb6a39d1e84b9d3c49cbf
Rajan Poudel <raajan.works@gmail.com>
2025-11-10 14:04:05 +0545

    Add files via upload

---
+++ PHP/Self Practice/Hacker Rank Solution/HackerRanktimeConversion.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion($s) {
+    // Write your code here
+    $t = date('H:i:s', strtotime($s));
+    return $t;
+
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$s = rtrim(fgets(STDIN), "\r\n");
+
+$result = timeConversion($s);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
