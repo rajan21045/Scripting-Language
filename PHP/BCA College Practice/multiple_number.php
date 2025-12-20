08919874ac3270f9110cf315fd6819c6c5d5b391
Rajan Poudel <raajan.works@gmail.com>
2025-12-20 23:02:05 +0545

    Update multiple_number.php

--- PHP/BCA College Practice/multiple_number.php
+++ PHP/BCA College Practice/multiple_number.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
+</head> 
 <body>
     <?php
     if($_SERVER['REQUEST_METHOD']== 'POST'){
